9ff440774b31072fabf18cc8d01815836ea58a4f
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2022-05-18 19:35:18 +0430

    chore: upgrade tailwindcss and its related packages
    - Get rid of postcss-preset-env because it has conflicts with
    the new postcss cli

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-    plugins: [
-        "tailwindcss",
-        "autoprefixer",
-        ["postcss-preset-env", { stage: 1 }],
-    ],
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
 };
