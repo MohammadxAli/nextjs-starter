c3af2d1d64ff9f57093fb22094d7e618a42395e6
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2022-05-20 21:15:39 +0430

    chore: make sure we have at least one locale by default

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,15 +1,15 @@
 const HttpBackend = require("i18next-http-backend/cjs");
 const path = require("path");
 
-const defaultLocale = process.env.NEXT_PUBLIC_DEFAULT_LOCALE;
+const defaultLocale = process.env.NEXT_PUBLIC_DEFAULT_LOCALE || "en";
 
 module.exports = {
     i18n: {
         defaultLocale,
-        locales: [defaultLocale, "en"],
+        locales: [defaultLocale, "fa"],
         localeDetection: false,
         localePath: path.resolve("./public/locales"),
     },
-    use: process.browser ? [HttpBackend] : [],
+    use: typeof window === "undefined" ? [] : [HttpBackend],
     serializeConfig: false,
 };
