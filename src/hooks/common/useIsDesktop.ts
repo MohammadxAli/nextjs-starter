d6ab883d6da45237e20d491dc2b58901bf937068
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2022-05-20 21:12:16 +0430

    refactor: remove `useIsDesktop` since we have `useBreakpoint` hooks

--- src/hooks/common/useIsDesktop.ts
+++
@@ -1,8 +0,0 @@
-import { useMediaQuery, useTheme } from "@mui/material";
-
-const useIsDesktop = () => {
-    const theme = useTheme();
-    return useMediaQuery(theme.breakpoints.up("lg"));
-};
-
-export default useIsDesktop;
